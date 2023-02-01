f279dee2ade6086dc7523f959a5c108b8fcb598f
hyukee0205 <hyukee0205@gmail.com>
2023-02-01 14:28:06 +0900

    0201_2

--- todo_list/src/components/Todo/Todo.jsx
+++ todo_list/src/components/Todo/Todo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {FaTrashAlt} from 'react-icons/fa';
+import styles from './Todo.module.css';
+
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status } = todo;
@@ -9,16 +11,17 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
-      <input
+    <li className={styles.todo}>
+      <input 
+        className={styles.checkbox}
         type='checkbox'
         id='checkbox'
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label htmlFor='checkbox'>{text}</label>
-      <span>
-        <button onClick={handleDelete}>
+      <label htmlFor='checkbox' className={styles.text}>{text}</label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
           <FaTrashAlt />
         </button>
       </span>
