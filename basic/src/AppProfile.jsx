9d2e479a0896aae4bae31dcfc96dfa1395cab87b
전찬혁 <hyukee0205@gmail.com>
2023-02-23 22:40:57 +0900

    props 상태값 적용

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -5,7 +5,22 @@ function AppProfile() {
 
   return (
     <>
-      <Profile />
+      <Profile 
+        image='https://i.pinimg.com/564x/f7/1d/60/f71d60f5abcf4d4cebc4dc33dd44c925.jpg'
+        name='chanhyuk Jeon'
+        title='프론트엔드 개발자'
+        isNew={true}
+      />
+      <Profile 
+        image='https://i.pinimg.com/564x/f7/1d/60/f71d60f5abcf4d4cebc4dc33dd44c925.jpg'
+        name='전찬혁1'
+        title='백엔드 개발자'
+      />
+      <Profile 
+        image='https://i.pinimg.com/564x/f7/1d/60/f71d60f5abcf4d4cebc4dc33dd44c925.jpg'
+        name='전찬혁3'
+        title='풀스택 개발자'
+      />
     </>
   );
 }
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,7 +1,17 @@
-export default function Profile() {
-  return <h1>Profile</h1>;
-}
+import React from 'react';
 
+export default function Profile({image, name, title, isNew}) {
+  return (
+    <div className='profile'>
+      <img className='photo' 
+        src={image}
+        alt="avatar" />
+      {isNew && <span className='new'>NEW</span>}
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
+}
 
 
 
