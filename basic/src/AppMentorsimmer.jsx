4c1fd9b3e8ed3fdb9cc19217c7d50b8de8994bf8
전찬혁 <hyukee0205@gmail.com>
2023-02-26 19:59:54 +0900

    immer

--- basic/src/AppMentorsimmer.jsx
+++ basic/src/AppMentorsimmer.jsx
@@ -1,31 +1,22 @@
 import React, { useReducer, useState } from 'react';
-import {useImmer} from 'use-immer'; 
 
 export default function AppMentor() {
   // const [person, setPerson] = useState(initialPerson);
-  const [person, updatePerson] = useImmer(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
 
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    updatePerson((person) => {
-      const mentor = person.mentors.find((m) => m.name === prev);
-      mentor.name = current;
-    })
+    dispatch({type: 'updated', prev, current});  
   };
   const handleAdd = () => {
     const name = prompt(`멘토의 이름은?`); 
     const title = prompt(`멘토의 직함은?`);
-    updatePerson((person) => {
-      person.mentors.push({name, title})
-    })
+    dispatch({type: 'added', name, title});  
   };
   const handleDelete = () => {
     const name = prompt(`누구를 삭제하고 싶은가요?`);
-    updatePerson((person) => {
-      const index = person.mentors.findIndex((m) => m.name === name);
-      person.mentors.splice(index, 1)
-    })
+    dispatch({type: 'added', name});  
   };
   return (
     <div>
