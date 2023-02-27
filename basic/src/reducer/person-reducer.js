935410281de155ad603117b9a07d193afde8ba56
전찬혁 <hyukee0205@gmail.com>
2023-02-28 00:24:37 +0900

    0228

--- basic/src/reducer/person-reducer.js
+++ basic/src/reducer/person-reducer.js
@@ -1,32 +1,32 @@
 export default function personReducer(person, action) {
   switch (action.type) {
     case 'updated': {
-      const {prev, current} = action;
+      const { prev, current } = action;
       return {
-        ...person, mentors: person.mentors.map((mentor) => {
-          if(mentor.name === prev) {
-            return {...mentor, name: current};
+        ...person,
+        mentors: person.mentors.map((mentor) => {
+          if (mentor.name === prev) {
+            return { ...mentor, name: current };
           }
-          return mentor; 
+          return mentor;
         }),
       };
     }
     case 'added': {
-      const {name, title} = action;
+      const { name, title } = action;
       return {
         ...person,
-        mentors: [{ name, title }, ...person.mentors],
+        mentors: [...person.mentors, { name, title }],
       };
     }
     case 'deleted': {
-       return {
-          ...person,
-          mentors: person.mentors.filter((m) => m.name !== name),
-        };
-      }
-      default: {
-        throw Error(`알수없는 액션 타입이다: ${action.type}`)
-      }
-
+      return {
+        ...person,
+        mentors: person.mentors.filter((mentor) => mentor.name !== action.name),
+      };
+    }
+    default: {
+      throw Error(`알수없는 액션 타입이다: ${action.type}`);
     }
   }
+}
