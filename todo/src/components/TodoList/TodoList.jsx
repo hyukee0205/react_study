7873da0b6033cf20a7db7209c5a5fac77f5fc415
전찬혁 <hyukee0205@gmail.com>
2023-03-03 15:48:16 +0900

    style add

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({filter}) {
+
   const [todos, setTodos] = useState([
     {id: '123', text: '장보기', status: 'active'},
     {id: '124', text: '공부하기', status: 'active'},
@@ -18,10 +19,12 @@ export default function TodoList() {
     setTodos(todos.filter((t) => (t.id !== deleted.id)));
   }
 
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
             <Todo 
             key={item.id} 
             todo={item} 
@@ -32,14 +35,17 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd}/>
 
       <button>삭제</button>
-      
 
     </section>
   );
 }
 
-
-
+function getFilteredItems(todos, filter) {
+  if(filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
+}
 
 
 
