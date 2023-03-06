336056aba2ffbd32d077355865871ee7fb4f157a
전찬혁 <hyukee0205@gmail.com>
2023-03-07 01:15:23 +0900

    json

--- youtube/.pnp.cjs
+++ youtube/.pnp.cjs
@@ -28,6 +28,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         [null, {\
           "packageLocation": "./",\
           "packageDependencies": [\
+            ["@tanstack/react-query", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:4.26.0"],\
             ["@testing-library/jest-dom", "npm:5.16.5"],\
             ["@testing-library/react", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:13.5.0"],\
@@ -4121,6 +4122,47 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@svgr/plugin-jsx", "npm:5.5.0"],\
             ["@svgr/plugin-svgo", "npm:5.5.0"],\
             ["loader-utils", "npm:2.0.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@tanstack/query-core", [\
+        ["npm:4.26.0", {\
+          "packageLocation": "./.yarn/cache/@tanstack-query-core-npm-4.26.0-817ab88cd7-cd5da19b78.zip/node_modules/@tanstack/query-core/",\
+          "packageDependencies": [\
+            ["@tanstack/query-core", "npm:4.26.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@tanstack/react-query", [\
+        ["npm:4.26.0", {\
+          "packageLocation": "./.yarn/cache/@tanstack-react-query-npm-4.26.0-9e806a9fee-4fcfe1fdd1.zip/node_modules/@tanstack/react-query/",\
+          "packageDependencies": [\
+            ["@tanstack/react-query", "npm:4.26.0"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:4.26.0", {\
+          "packageLocation": "./.yarn/__virtual__/@tanstack-react-query-virtual-ea3d349104/0/cache/@tanstack-react-query-npm-4.26.0-9e806a9fee-4fcfe1fdd1.zip/node_modules/@tanstack/react-query/",\
+          "packageDependencies": [\
+            ["@tanstack/react-query", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:4.26.0"],\
+            ["@tanstack/query-core", "npm:4.26.0"],\
+            ["@types/react", null],\
+            ["@types/react-dom", null],\
+            ["@types/react-native", null],\
+            ["react", "npm:18.2.0"],\
+            ["react-dom", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:18.2.0"],\
+            ["react-native", null],\
+            ["use-sync-external-store", "virtual:ea3d349104d8e5c70aa6cd0ec3ae4424f6c01bfc740607f581e2096e5b34ed63dedf27937755f40bee163c29363e9d70c34b580e44039bc38e5f4981ea3ebb6a#npm:1.2.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/react-dom",\
+            "@types/react-native",\
+            "@types/react",\
+            "react-dom",\
+            "react-native",\
+            "react"\
           ],\
           "linkType": "HARD"\
         }]\
@@ -15151,6 +15193,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./",\
           "packageDependencies": [\
             ["router", "workspace:."],\
+            ["@tanstack/react-query", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:4.26.0"],\
             ["@testing-library/jest-dom", "npm:5.16.5"],\
             ["@testing-library/react", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:6171214c9b4361540e80310e248f3ed6c99c8f282dbc3f66296cf2e75026f2b4af26ad280a294863bdf16aea9f92b0b9d61e1834f3368c88232eb192c5318104#npm:13.5.0"],\
@@ -16675,6 +16718,28 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["url-parse", "npm:1.5.10"],\
             ["querystringify", "npm:2.2.0"],\
             ["requires-port", "npm:1.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["use-sync-external-store", [\
+        ["npm:1.2.0", {\
+          "packageLocation": "./.yarn/cache/use-sync-external-store-npm-1.2.0-44f75d2564-5c639e0f8d.zip/node_modules/use-sync-external-store/",\
+          "packageDependencies": [\
+            ["use-sync-external-store", "npm:1.2.0"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:ea3d349104d8e5c70aa6cd0ec3ae4424f6c01bfc740607f581e2096e5b34ed63dedf27937755f40bee163c29363e9d70c34b580e44039bc38e5f4981ea3ebb6a#npm:1.2.0", {\
+          "packageLocation": "./.yarn/__virtual__/use-sync-external-store-virtual-8a7eff05d6/0/cache/use-sync-external-store-npm-1.2.0-44f75d2564-5c639e0f8d.zip/node_modules/use-sync-external-store/",\
+          "packageDependencies": [\
+            ["use-sync-external-store", "virtual:ea3d349104d8e5c70aa6cd0ec3ae4424f6c01bfc740607f581e2096e5b34ed63dedf27937755f40bee163c29363e9d70c34b580e44039bc38e5f4981ea3ebb6a#npm:1.2.0"],\
+            ["@types/react", null],\
+            ["react", "npm:18.2.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/react",\
+            "react"\
           ],\
           "linkType": "HARD"\
         }]\
--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import {useQuery} from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
   const {keyword} = useParams();
+  const {isLoading, error, data: videos
+  } = useQuery(['videos', keyword], async () => {
+      return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
   return (
-    <div>
-      Videos {keyword ? `${keyword}` : "hot trend, 검색결과 없음"}
-    </div>
+    <>
+      <div>
+        Videos {keyword ? `${keyword}` : "hot trend, 검색결과 없음"}
+      </div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Someting is wrong ..!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+        )}
+    </>
   );
-}
+};
 
