8819700d20f5e3de6d2f270e6bd440e38241ddfa
전찬혁 <hyukee0205@gmail.com>
2023-03-13 15:50:34 +0900

    커스텀 hook 추가

--- test/src/pages/NewProduct.jsx
+++ test/src/pages/NewProduct.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
-import Button from '../components/ui/Button'
+import Button from '../components/ui/Button';
+import useProducts from '../hooks/useProducts';
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsuploading] = useState(false);
   const [success, setSuccess] = useState();
+  
+  const {addProduct} = useProducts();
+
+
 
   const handleChange = (e) => {
     const {name, value, files} = e.target;
@@ -23,14 +27,12 @@ export default function NewProduct() {
     setIsuploading(true);
     uploadImage(file)
       .then(url => {
-        console.log(url);
-        addNewProduct(product, url)
-          .then(() => {
-            setSuccess('성공적으로 제품이 추가되었습니다. !');
-            setTimeout(() => {
-              setSuccess(null);
-            }, 2000);
-          });
+        addProduct.mutate({product, url}, {onSuccess: () => {
+          setSuccess('성공적으로 제품이 추가되었습니다. !');
+          setTimeout(() => {
+            setSuccess(null);
+          }, 2000);
+        }})
       })
       .finally(() => setIsuploading(false));
   }; 
