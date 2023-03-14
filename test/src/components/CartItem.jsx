1dc9de9872858310f41b529758e8f15258ae785d
전찬혁 <hyukee0205@gmail.com>
2023-03-14 23:55:56 +0900

    0314

--- test/src/components/CartItem.jsx
+++ test/src/components/CartItem.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {AiOutlineMinusSquare, AiOutlinePlusSquare} from 'react-icons/ai';
-import {RiDeleteBin5Fill} from 'react-icons/ri';
-import { addOrUpdateToCard, removeFromCart } from '../api/firebase';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { AiOutlinePlusSquare, AiOutlineMinusSquare } from 'react-icons/ai';
+import useCart from '../hooks/useCart';
 
-const ICON_CLASS = 'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
+const ICON_CLASS =
+  'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
 
 export default function CartItem({
-  product, 
-  product: {id, image, title, option, quantity, price},
-  uid
+  product,
+  product: { id, image, title, option, quantity, price },
 }) {
+  const { addOrUpdateItem, removeItem } = useCart();
   const handleMinus = () => {
-    if(quantity < 2) return;
-    addOrUpdateToCard(uid, {...product, quantity: quantity-1});
-  }
-  const handlePlus =  () => {
-    addOrUpdateToCard(uid, {...product, quantity: quantity+1});
-  }
-  const handleDelete = () => removeFromCart(uid, id);
+    if (quantity < 2) return;
+    addOrUpdateItem.mutate({ ...product, quantity: quantity - 1 });
+  };
+  const handlePlus = () =>
+    addOrUpdateItem.mutate({ ...product, quantity: quantity + 1 });
 
+  const handleDelete = () => removeItem.mutate(id);
 
   return (
     <li className='flex justify-between my-2 items-center'>
@@ -27,16 +27,15 @@ export default function CartItem({
         <div className='basis-3/5'>
           <p className='text-lg'>{title}</p>
           <p className='text-xl font-bold text-brand'>{option}</p>
-          <p>{price}</p>
+          <p>₩{price}</p>
         </div>
-        <div className='text-2xl flex itmes-center'>
+        <div className='text-2xl flex items-center'>
           <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
           <span>{quantity}</span>
-          <AiOutlinePlusSquare className={ICON_CLASS}  onClick={handlePlus} />
+          <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus} />
           <RiDeleteBin5Fill className={ICON_CLASS} onClick={handleDelete} />
         </div>
       </div>
     </li>
   );
 }
-
