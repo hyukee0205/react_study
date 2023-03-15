d350456cfc93b38d222951e759e3d1c52d20e77f
전찬혁 <hyukee0205@gmail.com>
2023-03-16 03:02:52 +0900

    헤더

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
+
 
 
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+  
   return (
-    <div>
-      <Link to='/'>
-        <h1>Project</h1>
-      </Link>      
-    </div>
+    <header className='flex justify-between border-b p-4'>
+      <Link to='/' className='flex'>
+        <h1 className='font-bold text-5xl'>Project</h1>
+      </Link>
+      <nav className='flex items-center gap-4 font-semibold text-xl'>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new'>상품등록!</Link>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
+      </nav>
+    </header>
   );
 }
 
