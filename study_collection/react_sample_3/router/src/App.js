0848c97071a24577c5a0e0927dd47f1f39cd7967
전찬혁 <hyukee0205@gmail.com>
2023-04-24 22:58:34 +0900

    0424

---
+++ study_collection/react_sample_3/router/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './pages/Home';
+import Videos from './pages/Videos';
+import NotFound from './pages/NotFound';
+import Root from './pages/Root';
+import VideoDetail from './pages/VideoDetail';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, element: <Home />},
+      {path: '/videos', element: <Videos />},
+      {path: '/videos/:videoId', element: <VideoDetail />},
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
