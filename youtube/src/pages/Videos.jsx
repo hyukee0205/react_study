b7a7b349a9dad51f9010e817f710bb1e69c2497f
전찬혁 <hyukee0205@gmail.com>
2023-04-30 02:52:44 +0900

    header 설정

--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 
 export default function Videos() {
-  return <div>Videos</div>;
+  const {keyword} = useParams();
+  return (
+    <div>
+      Videos {keyword ? `검색결과:${keyword}` : '키워드가 없습니다. 핫트렌드를 보여드릴게요!'}
+    </div>
+  );
 }
