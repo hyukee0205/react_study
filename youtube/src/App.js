b001b1f9a654376c6fac719605b06dafb935a8c4
전찬혁 <hyukee0205@gmail.com>
2023-04-29 23:45:24 +0900

    youtube api 연습

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import SearchHeader from './components/SearchHeader';
 
 function App() {
   return (
     <>
-      <SearchHeader />
       <Outlet />
     </>
   );
